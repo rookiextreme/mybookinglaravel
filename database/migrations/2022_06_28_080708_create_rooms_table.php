492110ca9a2695c84c1e1c321fcbaaa5ff65c30c
nurul adha <nuruladhasirajuddin17@gmail.com>
2022-07-15 11:53:58 +0800

    lupa masuk migration

---
+++ database/migrations/2022_06_28_080708_create_rooms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('aras');
+            $table->integer('kapasiti');
+            $table->string('penerangan');
+            $table->string('kemudahan');
+            $table->integer('delete_id');
+            $table->integer('fllag');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
